1bb62f1b69f51e2fed168aa2f22ac86ca5fbe0ad
itw_niuzs <itw_niuzs@taikanglife.com>
2018-11-28 14:00:35 +0800

    Adding optional format suffixes to our URLs

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,9 +3,12 @@ __author__ = 'Niu Zhisheng'
 __date__ = '2018/11/28 11:53'
 
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
